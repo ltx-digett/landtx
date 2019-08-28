e0b463a56b7709d9901398c5100a81f2d9dc0ea9
Alexander Fountain <alexander@digett.com>
2019-08-27 19:47:47 -0500

    styled components

--- gatsby/src/components/regions/footer.js
+++ gatsby/src/components/regions/footer.js
@@ -1,56 +1,47 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import React from "react"
+import { Link } from "gatsby"
 import styled from "styled-components"
-import Container from '../container'
-import * as variable from '../variables'
-import digettlogo from '../../images/digett_white.png'
-
-
+import Container from "../container"
+import * as variable from "../variables"
+import digettlogo from "../../images/digett_white.png"
 
 const FooterStyle = styled.footer`
-    padding:40px 0px;
-    background-color:${variable.brown}
-    .footer-container{
-        display:flex;
-        justify-content:space-between;
-        align-items:center;
-        .copy{
-            width:50%;
-            color:white;
-        }
-        .digett{
-            width:calc(50% - 40px);
-            display:flex;
-            justify-content:flex-end;
-            img{
-                width:100px;
-            }
-        }
+  padding: 40px 0px;
+  background-color: ${variable.brown};
+  .footer-container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    .copy {
+      width: 50%;
+      color: white;
     }
-`;
-
-
-
-export const Footer = ({
-    mainmenu,
-  }) => {
-    return (
-        <FooterStyle className="ltx-footer">
-
-        <Container className="footer-container">
-            <div className="copy">
-                2019 - Culver/LANDTX, Inc. - All Rights Reserved
-            </div>
-            <div className="digett">
-                <a href="https://www.digett.com">
-                    <img src={digettlogo}/>
-                </a>
-            </div>
-        </Container>
-    </FooterStyle>
-
-    )
+    .digett {
+      width: calc(50% - 40px);
+      display: flex;
+      justify-content: flex-end;
+      img {
+        width: 100px;
+      }
+    }
   }
+`
 
+export const Footer = ({ mainmenu }) => {
+  return (
+    <FooterStyle className="ltx-footer">
+      <Container className="footer-container">
+        <div className="copy">
+          2019 - Culver/LANDTX, Inc. - All Rights Reserved
+        </div>
+        <div className="digett">
+          <a href="https://www.digett.com">
+            <img src={digettlogo} />
+          </a>
+        </div>
+      </Container>
+    </FooterStyle>
+  )
+}
 
 export default Footer
