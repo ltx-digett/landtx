126dd262b1bad39e08320e64d063ed94da24b729
Alexander Fountain <alexander@digett.com>
2019-09-26 12:23:00 -0500

    es6 imports

--- gatsby/gatsby-browser.js
+++ gatsby/gatsby-browser.js
@@ -5,3 +5,7 @@
  */
 
 // You can delete this file if you're not using it
+import "core-js/modules/es6.set" // react-dom
+import "core-js/modules/es6.map" // react-dom
+import "core-js/modules/es6.string.starts-with" // redux-form
+import "raf/polyfill"
