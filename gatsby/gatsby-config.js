a3100dbd1f200ef1c570a534bf65f1e9000b494e
Alexander Fountain <alexander@digett.com>
2020-01-06 18:03:51 -0600

    manifest updates

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -145,13 +145,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `LANDTX`,
+        short_name: `lANDTX`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#2C2E29`,
+        theme_color: `#2C2E29`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `./src/images/fav.png`, // This path is relative to the root of the site.
       },
     },
     {
