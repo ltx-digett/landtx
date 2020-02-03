92481135d4742d87a9aa9fd91ef918b70daff166
Alexander Fountain <alexander@digett.com>
2020-02-03 09:45:42 -0600

    netlify build

---
+++ sanitylandtx/src/dashboardConfig.js
@@ -0,0 +1,18 @@
+export default {
+  widgets: [
+      {
+      name: 'netlify',
+      options: {
+        title: 'My Netlify deploys',
+        sites: [
+          {
+            title: 'Sanity Studio',
+            apiId: '0f6ab0ac-321b-4ae4-92bc-f9ffb0b36cd4',
+            buildHookId: '5d83de4f52ec0c018ffe345e',
+            name: 'landtx.com',
+          }
+        ]
+      }
+    }
+  ]
+}
