782aa20a0d1b325b770725f2c3d0f690b19c49de
Alexander Fountain <alexander@digett.com>
2020-02-03 10:27:33 -0600

    sanity updates for netlify build

--- sanitylandtx/src/dashboardConfig.js
+++ sanitylandtx/src/dashboardConfig.js
@@ -6,10 +6,16 @@ export default {
         title: 'My Netlify deploys',
         sites: [
           {
-            title: 'Sanity Studio',
+            title: 'landtx.com',
             apiId: '0f6ab0ac-321b-4ae4-92bc-f9ffb0b36cd4',
             buildHookId: '5d83de4f52ec0c018ffe345e',
-            name: 'landtx.com',
+            name: 'landtx',
+          },
+          {
+            title: 'dev.landtx.com',
+            apiId: '0f6ab0ac-321b-4ae4-92bc-f9ffb0b36cd4',
+            buildHookId: '5dc45cb2f997a60193204e3f',
+            name: 'dev.landtx',
           }
         ]
       }
